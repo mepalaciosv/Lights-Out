aec04c6013fb27e9db6665cc35b955a6e9a3ad9c
aromeroTUL <fiorenttos227@gmail.com>
2021-07-20 17:16:45 -0500

    some updates and practice

--- primerproyecto/src/App.jsx
+++ primerproyecto/src/App.jsx
@@ -1,17 +1,32 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Inicio  from './components/Inicio'
 import Estadisticas from './components/Estadisticas'
 import Desarrolladores from './components/Desarrolladores'
 import Login from './components/Login'
 
+// Tests
+// import Contador from './components/Contador'
+// import Listado from './components/Listado'
+import Temperatura from './components/Temperatura'
+// import Formulario from './components/Formulario'
+import ListadoNombres from './components/ListadoNombres'
+
 function App() {
   return (
     <div className="App">
 
-      <Inicio> y aqui que onda </Inicio>
-      <Login> </Login>
-      <Estadisticas> </Estadisticas>      
+        <Inicio />
+        {/* <Contador></Contador> */}
+        {/* <Listado></Listado> */}
+        <Temperatura></Temperatura>
+        {/* <Formulario></Formulario> */}
+
+        <h3> f </h3>
+        <ListadoNombres />
+      {/* <Login> </Login> */}
+      {/* <Estadisticas> </Estadisticas>*/}
       {/* <Desarrolladores> </Desarrolladores> */}
 
     </div>
