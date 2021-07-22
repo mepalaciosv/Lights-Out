2b2b1bbf2da6599314383a249da3f2fe25a22193
aromeroTUL <fiorenttos227@gmail.com>
2021-07-22 18:38:03 -0500

    dinamic board

--- primerproyecto/src/components/Desarrolladores.jsx
+++ primerproyecto/src/components/Desarrolladores.jsx
@@ -11,8 +11,6 @@ const Desarrolladores = () => {
           <h1 class="jumbotron-heading">Desarrolladores</h1>
           <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
           <p>
-            <a href="#" class="btn btn-primary my-2">Contacto</a>
-            <a href="#" class="btn btn-secondary my-2">Donaciones</a>
           </p>
         </div>
       </section>
--- primerproyecto/src/components/Inicio.jsx
+++ primerproyecto/src/components/Inicio.jsx
@@ -1,48 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Inicio = () => {
 
     // Antes del return se puede poner codigo JS
+
+    const [dimensions, setDimensions] = useState([4,4])
+    const [array, setArray] = useState([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+
+    const createArray = (e)=> {
+        e.preventDefault()
+        console.log('se creo el array')
+        const newArray = Array(dimensions[0]).fill(0).map(row => new Array(dimensions[1]).fill(0))
+        setArray(newArray)
+
+    }
+
+    const changeColor = (a,b) => {
+
+        const aN = Number(a)
+        const bN = Number(b)
+        const newArray = [...array]
+
+        if (newArray[aN][bN] === 1) {
+            newArray[aN][bN] = 0
+        } else {
+            newArray[aN][bN] = 1
+        }
+        
+        setArray( newArray )  
+    }
+
+    const changeDimensions = (a) => {
+        setDimensions([a,a])
+    }
     
     return (
 
         // En el return se puede escribir codigo .html
+        <div>
+            {/* Acá se especifica el tamaño del tablero */}
+            <div>
+            <form onSubmit={createArray}>
+                <input 
+                    placeholder = "Ingresa el tamaño"
+                    className = "form-control mb-3"
+                    type = "text"
+                    onChange = {(e) => {changeDimensions(Number(e.target.value))}}
+                />
+                {/* Boton de submit */}
+                <input 
+                        className = "btn btn-info btn-block mb-3"
+                        type = "submit"
+                />
 
-        <div class="m-0 row justify-content-center align-items-center">    
-            <div class = "col-auto p-5 text-center">     
-            <div class="row">          
-                <div class="col">
-                    <button id = "1" type="button" onClick={ () => { document.getElementById("1").style.background === "yellow" ? document.getElementById("1").style.background = "black": document.getElementById("1").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "2" type="button" onClick={ () => { document.getElementById("2").style.background === "yellow" ? document.getElementById("2").style.background = "black": document.getElementById("2").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "3" type="button" onClick={ () => { document.getElementById("3").style.background === "yellow" ? document.getElementById("3").style.background = "black": document.getElementById("3").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "4" type="button" onClick={ () => { document.getElementById("4").style.background === "yellow" ? document.getElementById("4").style.background = "black": document.getElementById("4").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                </div>
+            </form>
+                
             </div>
-            <div class="row">
-                <div class="col">
-                    <button id = "5" type="button" onClick={ () => { document.getElementById("5").style.background === "yellow" ? document.getElementById("5").style.background = "black": document.getElementById("5").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "6" type="button" onClick={ () => { document.getElementById("6").style.background === "yellow" ? document.getElementById("6").style.background = "black": document.getElementById("6").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "7" type="button" onClick={ () => { document.getElementById("7").style.background === "yellow" ? document.getElementById("7").style.background = "black": document.getElementById("7").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "8" type="button" onClick={ () => { document.getElementById("8").style.background === "yellow" ? document.getElementById("8").style.background = "black": document.getElementById("8").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                </div>
+
+            {/* Tablero de juego que se crea de forma dinamica */}
+            <div className="game-board">     
+                {
+                    array.map( (item, key) =>
+                        <div>
+                            {
+                                Object.keys(item).map( (item2, key2) =>
+                                    <div>
+                                        <button className = {array[key][key2] === 1 ? 'on-button' : 'off-button' } 
+                                        type="button" onClick = { () => {changeColor(key,key2)} } > 
+                                            Light 
+                                        </button>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    )
+                }
             </div>
-            <div class="row">
-                <div class="col">
-                    <button id = "9" type="button" onClick={ () => { document.getElementById("9").style.background === "yellow" ? document.getElementById("9").style.background = "black": document.getElementById("9").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "10" type="button" onClick={ () => { document.getElementById("10").style.background === "yellow" ? document.getElementById("10").style.background = "black": document.getElementById("10").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "11" type="button" onClick={ () => { document.getElementById("11").style.background === "yellow" ? document.getElementById("11").style.background = "black": document.getElementById("11").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "12" type="button" onClick={ () => { document.getElementById("12").style.background === "yellow" ? document.getElementById("12").style.background = "black": document.getElementById("12").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <button id = "13" type="button" onClick={ () => { document.getElementById("13").style.background === "yellow" ? document.getElementById("13").style.background = "black": document.getElementById("13").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "14" type="button" onClick={ () => { document.getElementById("14").style.background === "yellow" ? document.getElementById("14").style.background = "black": document.getElementById("14").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "15" type="button" onClick={ () => { document.getElementById("15").style.background === "yellow" ? document.getElementById("15").style.background = "black": document.getElementById("15").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                    <button id = "16" type="button" onClick={ () => { document.getElementById("16").style.background === "yellow" ? document.getElementById("16").style.background = "black": document.getElementById("16").style.background = "yellow"} } class="btn btn-dark p-4 rounded-0"> Light </button>
-                </div>
-            </div>
-            </div> 
         </div>
     )
 }
