529ebf39c536a6be6b340d34cf2275a97b4119b1
Margarita Palacios <mepalaciosv@gmail.com>
2021-07-23 23:09:29 -0500

    Cambios varios

--- primerproyecto/src/components/Desarrolladores.jsx
+++ primerproyecto/src/components/Desarrolladores.jsx
@@ -8,7 +8,7 @@ const Desarrolladores = () => {
 
       <section class="jumbotron text-center">
         <div class="container">
-          <h1 class="jumbotron-heading">Desarrolladores</h1>
+          <h1 class="jumbotron-heading">Equipo</h1>
           <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
           <p>
           </p>
