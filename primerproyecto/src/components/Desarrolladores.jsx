a4de69c89fb4f7b798ffae901430a490d2143737
Margarita Palacios <mepalaciosv@gmail.com>
2021-07-23 23:10:26 -0500

    Cambios varios

--- primerproyecto/src/components/Desarrolladores.jsx
+++ primerproyecto/src/components/Desarrolladores.jsx
@@ -4,31 +4,31 @@ const Desarrolladores = () => {
     return (
         <div>
             
-    <main role="main">
+    <main role = "main">
 
-      <section class="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">Equipo</h1>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+      <section class = "jumbotron text-center">
+        <div class = "container">
+          <h1 class = "jumbotron-heading">Equipo</h1>
+          <p class = "lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
           <p>
           </p>
         </div>
       </section>
 
-      <div class="album py-5 bg-light">
-        <div class="container">
+      <div class = "album py-5 bg-light">
+        <div class = "container">
 
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-              <img src="https://img.icons8.com/pastel-glyph/64/000000/person-male--v3.png" alt = "Foto" />
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Details</button>
+          <div class = "row">
+            <div class = "col-md-4">
+              <div class = "card mb-4 box-shadow">
+              <img src = "https://img.icons8.com/pastel-glyph/64/000000/person-male--v3.png" alt = "Foto" />
+                <div class = "card-body">
+                  <p class = "card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class = "d-flex justify-content-between align-items-center">
+                    <div class = "btn-group">
+                      <button type = "button" class="btn btn-sm btn-outline-secondary">Details</button>
                     </div>
-                    <small class="text-muted">9 mins</small>
+                    <small class = "text-muted">9 mins</small>
                   </div>
                 </div>
               </div>
