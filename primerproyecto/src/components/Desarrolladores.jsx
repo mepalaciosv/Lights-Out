e1f2c12a5741b5f94f6d6c11af4e100c4994be9b
Margarita Palacios <mepalaciosv@gmail.com>
2021-07-25 00:56:28 -0500

    Pantalla de desarrolladores

--- primerproyecto/src/components/Desarrolladores.jsx
+++ primerproyecto/src/components/Desarrolladores.jsx
@@ -1,57 +1,31 @@
 import React from 'react'
+import margarita from './imgs/margarita.jpg';
+import andres from './imgs/andres.png';
+import equipo from './imgs/equipo.png'
+import "./Desarrolladores.css";
 
 const Desarrolladores = () => {
     return (
         <div>
-            
-    <main role = "main">
+          <div>
+            <img src = {equipo} className = "imagenSuperior" alt = "imagenSuperior"/>
+            <h1 className = "encabezado">Equipo</h1>
+          </div>
 
-      <section class = "jumbotron text-center">
-        <div class = "container">
-          <h1 class = "jumbotron-heading">Equipo</h1>
-          <p class = "lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-          <p>
-          </p>
-        </div>
-      </section>
-
-      <div class = "album py-5 bg-light">
-        <div class = "container">
-
-          <div class = "row">
-            <div class = "col-md-4">
-              <div class = "card mb-4 box-shadow">
-              <img src = "https://img.icons8.com/pastel-glyph/64/000000/person-male--v3.png" alt = "Foto" />
-                <div class = "card-body">
-                  <p class = "card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class = "d-flex justify-content-between align-items-center">
-                    <div class = "btn-group">
-                      <button type = "button" class="btn btn-sm btn-outline-secondary">Details</button>
-                    </div>
-                    <small class = "text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
+          <div class = "stats-container">
+            <div class = "column">
+              <img src = {andres} className = "imagenEquipo" alt = "imagenSuperior"/>
+              <h1 className = "desarrollador">Andrés Romero</h1>
+              <h1 className = "descripcion">Ingeniero Químico e industrial</h1>
+              <a className = "contacto" href="https://github.com/AndresFRomero">github.com/AndresFRomero</a>
             </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-              <img src="https://img.icons8.com/pastel-glyph/64/000000/person-male--v3.png" alt = "Foto"/>
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Details</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
+            <div class="column">
+            <img src = {margarita} className = "imagenEquipo" alt = "imagenSuperior"/>
+              <h1 className = "desarrollador">Margarita Palacios</h1>
+              <h1 className = "descripcion">Matemática e Ingeniera Mecánica</h1>
+              <a className = "contacto" href="https://github.com/mepalaciosv">github.com/mepalaciosv</a>
             </div>
           </div>
-        </div>
-      </div>
-
-    </main>
 
         </div>
     )
